ae5c7f457886dbf6f74c588b08ac3d2c8a0647ed
Larissa <larisssa_rga@hotmail.com>
2022-08-31 18:56:32 -0300

    Adiciona primeiro, segundo e terceiro requisito

---
+++ script.js
@@ -0,0 +1,10 @@
+// const colorPallet = document.getElementById('color-pallete')
+// function createPalletColors() {
+//     for (let index = 0; index < 4; index += 1) {
+//         const div = document.createElement('div');
+//         div.className = 'color';
+//         colorPallet.appendChild(div);
+        
+//     }
+// }
+// createPalletColors();
